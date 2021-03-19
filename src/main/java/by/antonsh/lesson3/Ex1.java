649e04ee64b539491fb7d86ac493bc91cf761579
СУРГ <anton2.bl32@gmail.com>
2021-03-19 15:17:00 +0300

    lesson3 add 1,2

---
+++ src/main/java/by/antonsh/lesson3/Ex1.java
@@ -0,0 +1,11 @@
+package by.antonsh.lesson3;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int km=10;
+        for (int i = 1; i < 7; i++) {
+            km *=1.1;
+        }
+        System.out.println("пробежал за 7 дней "+km+" км");
+    }
+}
---
+++ src/main/java/by/antonsh/lesson3/Ex2.java
@@ -0,0 +1,12 @@
+package by.antonsh.lesson3;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        int times=1;
+        for (int i = 3; i <= 24; i=i+3) {
+            times *=2;
+            System.out.println("Будет "+times+" амеб через "+i+" часов");
+        }
+        
+    }
+}
