4afe88159dc535e6d58e3c16826cf534e8e34bee
СУРГ <anton2.bl32@gmail.com>
2021-03-22 15:09:31 +0300

    add xml

--- src/main/java/by/antonsh/project/Library.java
+++ src/main/java/by/antonsh/project/Library.java
@@ -1,14 +1,24 @@
 package by.antonsh.project;
 
+import javax.xml.bind.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class Library {
     private static List<Book> myLibrary;
+    private static final String fileName="d:/mylibrary.xml";
 
     public static void main(String[] args) {
+        createBook();
+        saveMyLibrary();
+        System.out.println(myLibrary);
+        myLibrary.clear();
+        getMyLibrary();
+        System.out.println(myLibrary);
+
 
     }
     public List<Book> sortByName(){
@@ -21,7 +31,51 @@ public class Library {
         sorted.sort(Comparator.comparing(Book::getDateOfBook));
         return sorted;
     }
+    public static void saveMyLibrary(){
+        JAXBContext jaxbContext = null;
+        try {
+            jaxbContext = JAXBContext.newInstance(Book.class);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        Marshaller jaxbMarshaller = null;
+        try {
+            jaxbMarshaller = jaxbContext.createMarshaller();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        } catch (PropertyException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            jaxbMarshaller.marshal(myLibrary, new File(fileName));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public  static void getMyLibrary(){
+        JAXBContext jaxbContext = null;
+        try {
+            jaxbContext = JAXBContext.newInstance(Book.class);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        try {
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            myLibrary= (List<Book>) jaxbUnmarshaller.unmarshal( new File(fileName) );
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
 
 
-
+    }
+    public static void createBook(){
+        Book book=new Book("Три танкиста и собака",1991,new Author("Котеригин","Василий"),Genre.ACTION);
+        myLibrary.add(book);
+    }
 }
