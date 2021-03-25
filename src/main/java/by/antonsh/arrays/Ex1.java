4d2cf37865c03be36cb4a0a7d224711d3fb73218
СУРГ <anton2.bl32@gmail.com>
2021-03-25 16:06:48 +0300

    lesson 3 add exercise 4

--- src/main/java/by/antonsh/arrays/Ex1.java
+++ src/main/java/by/antonsh/arrays/Ex1.java
@@ -6,11 +6,11 @@ import java.util.Random;
 public class Ex1 {
     public static void main(String[] args) {
 
-        int[] randomArray=new int[10];
+        int[] randomArray = new int[10];
         for (int i = 0; i < 10; i++) {
-            randomArray[i]=i*2;
+            randomArray[i] = i * 2;
         }
-        Arrays.stream(randomArray).forEach(a-> System.out.print(a + " "));
+        Arrays.stream(randomArray).forEach(a -> System.out.print(a + " "));
         System.out.println();
         System.out.println("-------------------------");
         Arrays.stream(randomArray).forEach(System.out::println);
--- src/main/java/by/antonsh/arrays/Ex3.java
+++ src/main/java/by/antonsh/arrays/Ex3.java
@@ -5,15 +5,15 @@ import java.util.Random;
 
 public class Ex3 {
     public static void main(String[] args) {
-        Random random=new Random();
-        int[] array=new int[15];
+        Random random = new Random();
+        int[] array = new int[15];
 
         for (int i = 0; i < array.length; i++) {
-            array[i]= random.nextInt(99);
+            array[i] = random.nextInt(99);
         }
         System.out.println(Arrays.toString(array));
-        Arrays.stream(array).filter(a->a%2==0&&a>0).forEach(a-> System.out.print(a+"  "));
+        Arrays.stream(array).filter(a -> a % 2 == 0 && a > 0).forEach(a -> System.out.print(a + "  "));
         System.out.println();
-        System.out.println("Четных чисел в массиве " + Arrays.stream(array).filter(a->a%2==0&&a>0).count());
+        System.out.println("Четных чисел в массиве " + Arrays.stream(array).filter(a -> a % 2 == 0 && a > 0).count());
     }
 }
