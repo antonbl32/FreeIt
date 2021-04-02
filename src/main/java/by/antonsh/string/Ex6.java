d999a7fe3983618a9bf1dbacb8a8312682db9e0d
СУРГ <anton2.bl32@gmail.com>
2021-04-02 15:43:44 +0300

    add finish 6

--- src/main/java/by/antonsh/string/Ex6.java
+++ src/main/java/by/antonsh/string/Ex6.java
@@ -1,4 +1,7 @@
 package by.antonsh.string;
+
+import java.util.Arrays;
+
 /*
 Имеется строка с текстом. Вывести текст, составленный из последних букввсех слов
  */
@@ -8,7 +11,9 @@ public class Ex6 {
                 " то не имеет значения какое кол-во слов в предложении," +
                 "оно попадает в новый файл.Пишем все в \"ООП\" стиле. " +
                 "Создаём класс TextFormater в котором два статических метода:";
-
+        String[] textWithoutMarks=text.split("[?.,:\"]");
+        // Выведу  2 последние буквы слов, содержащих более 2 букв
+        Arrays.stream(textWithoutMarks).filter(a->a.length()>2).forEach(a-> System.out.println(a.substring(a.length()-2,a.length())));
     }
 
 
