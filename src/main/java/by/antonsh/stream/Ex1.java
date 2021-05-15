7fbe224a4c4c7c056404f4089beaf326230b48b5
antonbl32 <anton2.bl32@gmail.com>
2021-05-15 19:35:39 +0300

    some fix

--- src/main/java/by/antonsh/stream/Ex1.java
+++ src/main/java/by/antonsh/stream/Ex1.java
@@ -1,26 +1,19 @@
 package by.antonsh.stream;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 /*
 Создать свой функциональный интерфейс для вывода на консоль данных объекта.
 С помощью лямбда-выражения вывести данные для объектов типа String и Integer.
  */
 public class Ex1 {
     public static void main(String[] args) {
-        User user = new User("Denis", 13);
-        UserDetails userDetails = u -> u.toString();
-        System.out.println(userDetails.getDetails(user));
+        Details details = o -> System.out.println(o);
+        int i=1;
+        String k="Hello";
+        details.getDetails(i);
+        details.getDetails(k);
     }
 }
-@Data
-@AllArgsConstructor
-class User {
-    private String name;
-    private int age;
-}
-
 @FunctionalInterface
-interface UserDetails {
-    String getDetails(User user);
+interface Details <T> {
+    void getDetails(T t);
 }
