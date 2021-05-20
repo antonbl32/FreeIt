bc6c98ee0c320e1b24b8502d36b576e386c57952
antonbl32 <60536119+antonbl32@users.noreply.github.com>
2021-05-20 20:43:33 +0300

    Update Ex7.java

--- src/main/java/by/antonsh/lesson3/Ex7.java
+++ src/main/java/by/antonsh/lesson3/Ex7.java
@@ -2,10 +2,9 @@ package by.antonsh.lesson3;
 
 public class Ex7 {
     public static void main(String[] args) {
+        int summ=0;
         for (int i = 1; i <99 ; i++) {
-            if(i%2!=0){
-                System.out.println(i+" нечетное число");
-            }
+            summ +=i;
         }
     }
 }
